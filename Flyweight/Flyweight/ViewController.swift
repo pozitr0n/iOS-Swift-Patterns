5d845c5f782d41511f681857692994cbb78cf4f5
Raman Kozar <pozitr0n.kozarroman@gmail.com>
2023-10-31 03:28:56 +0100

    Added new pattern Flyweight

---
+++ Flyweight/Flyweight/ViewController.swift
@@ -0,0 +1,64 @@
+//
+//  ViewController.swift
+//  Flyweight
+//
+//  Created by Raman Kozar on 31/10/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let red: UIColor = .red
+        let red2: UIColor = .red
+        
+        print(red === red2)
+        
+        //
+        
+        let color = UIColor(red: 1, green: 0, blue: 0, alpha: 1)
+        let color2 = UIColor(red: 1, green: 0, blue: 0, alpha: 1)
+        
+        print(color === color2)
+        
+        //
+        
+        let flyColor = UIColor.rgba(1, 0, 0, 1)
+        let flyColor2 = UIColor.rgba(1, 0, 0, 1)
+        
+        print(flyColor === flyColor2)
+        
+    }
+
+}
+
+extension UIColor {
+    
+    static var colorStore: [String: UIColor] = [:]
+    
+    class func rgba(_ red: CGFloat, 
+                    _ green: CGFloat,
+                    _ blue: CGFloat,
+                    _ alpha: CGFloat) -> UIColor {
+    
+        let key: String = "\(red)\(green)\(blue)\(alpha)"
+        
+        if let color = colorStore[key] {
+            return color
+        }
+        
+        let color = UIColor(red: red,
+                            green: green,
+                            blue: blue,
+                            alpha: alpha)
+        
+        colorStore[key] = color
+        return color
+        
+    }
+    
+}
+

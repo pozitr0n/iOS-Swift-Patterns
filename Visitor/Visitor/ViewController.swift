32d32ffeba7a8d0a99b8ae47c605dbe0df37803e
Raman Kozar <pozitr0n.kozarroman@gmail.com>
2023-11-11 23:58:10 +0100

    Changes at Visitor pattern

--- Visitor/Visitor/ViewController.swift
+++ Visitor/Visitor/ViewController.swift
@@ -46,8 +46,10 @@ class ViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         registerCell()
+        
     }
 
     func registerCell() {
